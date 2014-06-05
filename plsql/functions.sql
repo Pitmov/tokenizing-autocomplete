77476a259ac56e27c1803952deb9af7f6ab4dc65
pitmov <pitmov@gmail.com>
2014-06-05 19:26:31 +0400

    add new function set_data to set data async via ajax added pl/sql functions

--- plsql/functions.sql
+++ plsql/functions.sql
@@ -1,113 +1,178 @@
-FUNCTION RENDER (
-    P_ITEM                IN APEX_PLUGIN.T_PAGE_ITEM,
-    P_PLUGIN              IN APEX_PLUGIN.T_PLUGIN,
-    P_VALUE               IN VARCHAR2,
-    P_IS_READONLY         IN BOOLEAN,
-    P_IS_PRINTER_FRIENDLY IN BOOLEAN )
-  RETURN APEX_PLUGIN.T_PAGE_ITEM_RENDER_RESULT
-IS
-  L_RETURN APEX_PLUGIN.T_PAGE_ITEM_RENDER_RESULT;
-  L_CURSOR NUMBER;
-  L_COL_CNT PLS_INTEGER;
-  REC_TAB DBMS_SQL.DESC_TAB;
-  L_SQL VARCHAR2(32000);
-  L_NAME VARCHAR2 ( 30 );
-  L_LOV  VARCHAR2 ( 32000 ) := P_ITEM.LOV_DEFINITION; 
+FUNCTION render(p_item                IN apex_plugin.t_page_item,
+                p_plugin              IN apex_plugin.t_plugin,
+                p_value               IN VARCHAR2,
+                p_is_readonly         IN BOOLEAN,
+                p_is_printer_friendly IN BOOLEAN)
+  RETURN apex_plugin.t_page_item_render_result IS
+  l_return  apex_plugin.t_page_item_render_result;
+  l_cursor  NUMBER;
+  l_col_cnt PLS_INTEGER;
+  rec_tab   dbms_sql.desc_tab;
+  l_sql     VARCHAR2(32000);
+  l_name    VARCHAR2(30);
+  l_lov     VARCHAR2(32000) := p_item.lov_definition;
 BEGIN
   --
-  APEX_JAVASCRIPT.ADD_LIBRARY ( 
-   P_NAME => 'jquery.tokeninput', 
-   P_DIRECTORY => P_PLUGIN.FILE_PREFIX, 
-   P_VERSION => NULL ) ;
+  apex_javascript.add_library(p_name      => 'jquery.tokeninput',
+                              p_directory => p_plugin.file_prefix,
+                              p_version   => NULL);
   --
-  APEX_CSS.ADD_FILE ( 
-   P_NAME => 'token-input-facebook', 
-   P_DIRECTORY => P_PLUGIN.FILE_PREFIX, 
-   P_VERSION => NULL ) ;
+  apex_css.add_file(p_name      => 'token-input-facebook',
+                    p_directory => p_plugin.file_prefix,
+                    p_version   => NULL);
   --
-  L_NAME := APEX_PLUGIN.GET_INPUT_NAME_FOR_PAGE_ITEM ( FALSE ) ;
-  HTP.P ( '<input ' || 'name="' || L_NAME ||'" id="' || P_ITEM.NAME ||'" type="text" maxlength="4000" size="30" value="'||P_VALUE||'" class="text_field" />' ) ;
+  l_name := apex_plugin.get_input_name_for_page_item(FALSE);
+  htp.p('<input ' || 'name="' || l_name || '" id="' || p_item.name ||
+        '" type="text" maxlength="4000" size="30" value="' || p_value ||
+        '" class="text_field" />');
   --
-  L_CURSOR := DBMS_SQL.OPEN_CURSOR;
+  l_cursor := dbms_sql.open_cursor;
   --
-  DBMS_SQL.PARSE ( L_CURSOR, P_ITEM.LOV_DEFINITION, DBMS_SQL.NATIVE ) ;
-  DBMS_SQL.DESCRIBE_COLUMNS ( L_CURSOR, L_COL_CNT, REC_TAB ) ;
+  dbms_sql.parse(l_cursor,
+                 p_item.lov_definition,
+                 dbms_sql.native);
+  dbms_sql.describe_columns(l_cursor,
+                            l_col_cnt,
+                            rec_tab);
   --
-  DBMS_SQL.CLOSE_CURSOR ( L_CURSOR ) ;
+  dbms_sql.close_cursor(l_cursor);
   --
-  L_SQL := 'SELECT ' || REC_TAB ( 1 ).COL_NAME || ' AS "name", ';
-  L_SQL := L_SQL || REC_TAB ( 2 ).COL_NAME || ' AS "id" ';
-  IF TRIM (P_ITEM.ATTRIBUTE_08) IS NOT NULL THEN
-    L_LOV := REGEXP_REPLACE (P_ITEM.LOV_DEFINITION, REGEXP_REPLACE (TRIM (P_ITEM.ATTRIBUTE_08), '(SELECT|INSERT|UPDATE|DELETE|DROP|ALTER|CREATE)', '?', 1, 0, 'i'), SYS.HTF.ESCAPE_SC ( WWV_FLOW.G_X01 ), 1, 1, 'i');
+  l_sql := 'SELECT ' || rec_tab(1).col_name || ' AS "name", ';
+  l_sql := l_sql || rec_tab(2).col_name || ' AS "id" ';
+  IF TRIM(p_item.attribute_08) IS NOT NULL
+  THEN
+    l_lov := regexp_replace(p_item.lov_definition,
+                            regexp_replace(TRIM(p_item.attribute_08),
+                                           '(SELECT|INSERT|UPDATE|DELETE|DROP|ALTER|CREATE)',
+                                           '?',
+                                           1,
+                                           0,
+                                           'i'),
+                            sys.htf.escape_sc(wwv_flow.g_x01),
+                            1,
+                            1,
+                            'i');
   END IF;
-  L_SQL := L_SQL || 'FROM ( ' || L_LOV || ' ) '; 
-  L_SQL := L_SQL || 'WHERE INSTR( '','' || ''' || SYS.HTF.ESCAPE_SC ( P_VALUE ) || ''' || '','', '','' || "' || REC_TAB ( 2 ).COL_NAME || '" || '','', 1 ) > 0';
-  HTP.P ( '<script> ' || P_ITEM.NAME ||'_data =' );
-  APEX_UTIL.JSON_FROM_SQL ( L_SQL ) ;
-  HTP.P ( ';' );
-  HTP.P ( '</script>' ) ;
+  l_sql := l_sql || 'FROM ( ' || l_lov || ' ) ';
+  l_sql := l_sql || 'WHERE INSTR( '','' || ''' ||
+           sys.htf.escape_sc(p_value) || ''' || '','', '','' || "' || rec_tab(2)
+          .col_name || '" || '','', 1 ) > 0';
+  htp.p('<script> ' || p_item.name || '_data =');
+  apex_util.json_from_sql(l_sql);
+  htp.p(';');
+  htp.p('</script>');
   --
-  APEX_JAVASCRIPT.ADD_ONLOAD_CODE ( '$( "#' || P_ITEM.NAME || '").tokenInput( "wwv_flow.show",{
-  hintText: "'      || P_ITEM.ATTRIBUTE_02 || '",
-  noResultsText: "' || P_ITEM.ATTRIBUTE_03 || '",
-  searchingText: "' || P_ITEM.ATTRIBUTE_04 || '",
-  pluginId: "' ||apex_plugin.get_ajax_identifier || '",
-  allowNewValues: '|| CASE WHEN UPPER( P_ITEM.ATTRIBUTE_05 ) LIKE 'T%' THEN 'true' ELSE 'false' END || ',
-  canCreate: '     || CASE WHEN UPPER( P_ITEM.ATTRIBUTE_05 ) LIKE 'T%' THEN 'true' ELSE 'false' END || ',
-  createText: "'    || P_ITEM.ATTRIBUTE_06 || '",
+  apex_javascript.add_onload_code('$( "#' || p_item.name || '").tokenInput( "wwv_flow.show",{
+  hintText: "' || p_item.attribute_02 || '",
+  noResultsText: "' ||
+                                  p_item.attribute_03 || '",
+  searchingText: "' ||
+                                  p_item.attribute_04 || '",
+  pluginId: "' ||
+                                  apex_plugin.get_ajax_identifier || '",
+  allowNewValues: ' || CASE WHEN
+                                  upper(p_item.attribute_05) LIKE 'T%' THEN
+                                  'true' ELSE 'false'
+                                  END || ',
+  canCreate: ' || CASE WHEN
+                                  upper(p_item.attribute_05) LIKE 'T%' THEN
+                                  'true' ELSE 'false'
+                                  END || ',
+  createText: "' || p_item.attribute_06 || '",
   createIdentifier: "[NEW]",
   jsonContainer: "row",
-  initialValues: ' || P_ITEM.NAME || '_data.row,
-  queryParam: "p_flow_id=' || V ( 'APP_ID' ) || '&p_flow_step_id=' || V ( 'APP_PAGE_ID' ) || '&p_instance=' || V ( 'APP_SESSION' ) || '&p_request=PLUGIN=' || APEX_PLUGIN.GET_AJAX_IDENTIFIER || '&x01" } );' );
+  initialValues: ' || p_item.name ||
+                                  '_data.row,
+  queryParam: "p_flow_id=' || v('APP_ID') ||
+                                  '&p_flow_step_id=' || v('APP_PAGE_ID') ||
+                                  '&p_instance=' || v('APP_SESSION') ||
+                                  '&p_request=PLUGIN=' ||
+                                  apex_plugin.get_ajax_identifier ||
+                                  '&x01" } );');
   --
-  RETURN L_RETURN;
+  RETURN l_return;
   --
-END RENDER;
+END render;
 
-FUNCTION AJAX (
-    P_ITEM   IN APEX_PLUGIN.T_PAGE_ITEM,
-    P_PLUGIN IN APEX_PLUGIN.T_PLUGIN )
-  RETURN APEX_PLUGIN.T_PAGE_ITEM_AJAX_RESULT
-IS
-  L_RETURN APEX_PLUGIN.T_PAGE_ITEM_AJAX_RESULT;
-  L_CURSOR  INTEGER;
-  L_COL_CNT PLS_INTEGER;
-  L_REC_TAB DBMS_SQL.DESC_TAB;
-  L_SQL     VARCHAR2 ( 32000 ) ;
-  L_LOV  VARCHAR2 ( 32000 ) := P_ITEM.LOV_DEFINITION;
+FUNCTION ajax(p_item   IN apex_plugin.t_page_item,
+              p_plugin IN apex_plugin.t_plugin)
+  RETURN apex_plugin.t_page_item_ajax_result IS
+  l_return  apex_plugin.t_page_item_ajax_result;
+  l_cursor  INTEGER;
+  l_col_cnt PLS_INTEGER;
+  l_rec_tab dbms_sql.desc_tab;
+  l_sql     VARCHAR2(32000);
+  l_lov     VARCHAR2(32000) := p_item.lov_definition;
 BEGIN
-  WWV_FLOW.G_X01 := REPLACE(WWV_FLOW.G_X01,'''',''''''); 
+  wwv_flow.g_x01 := REPLACE(wwv_flow.g_x01,
+                            '''',
+                            '''''');
+  wwv_flow.g_x02 := REPLACE(wwv_flow.g_x02,
+                            '''',
+                            '''''');
   --
-  L_CURSOR := DBMS_SQL.OPEN_CURSOR;
+  l_cursor := dbms_sql.open_cursor;
   --
-  DBMS_SQL.PARSE ( L_CURSOR, P_ITEM.LOV_DEFINITION, DBMS_SQL.NATIVE ) ;
-  DBMS_SQL.DESCRIBE_COLUMNS ( L_CURSOR, L_COL_CNT, L_REC_TAB ) ;
+  dbms_sql.parse(l_cursor,
+                 p_item.lov_definition,
+                 dbms_sql.native);
+  dbms_sql.describe_columns(l_cursor,
+                            l_col_cnt,
+                            l_rec_tab);
   --
-  DBMS_SQL.CLOSE_CURSOR ( L_CURSOR ) ;
+  dbms_sql.close_cursor(l_cursor);
   --
-  L_SQL := 'SELECT ' || L_REC_TAB( 2 ).COL_NAME || ' AS "id", ';
-  L_SQL := L_SQL || L_REC_TAB( 1 ).COL_NAME || ' AS "name" ';
-  IF TRIM (P_ITEM.ATTRIBUTE_08) IS NOT NULL THEN
-    L_LOV := REGEXP_REPLACE (P_ITEM.LOV_DEFINITION, REGEXP_REPLACE (TRIM (P_ITEM.ATTRIBUTE_08), '(SELECT|INSERT|UPDATE|DELETE|DROP|ALTER|CREATE)', '?', 1, 0, 'i'), SYS.HTF.ESCAPE_SC ( WWV_FLOW.G_X01 ), 1, 1, 'i');
+  l_sql := 'SELECT ' || l_rec_tab(2).col_name || ' AS "id", ';
+  l_sql := l_sql || l_rec_tab(1).col_name || ' AS "name" ';
+  IF TRIM(p_item.attribute_08) IS NOT NULL
+  THEN
+    l_lov := regexp_replace(p_item.lov_definition,
+                            regexp_replace(TRIM(p_item.attribute_08),
+                                           '(SELECT|INSERT|UPDATE|DELETE|DROP|ALTER|CREATE)',
+                                           '?',
+                                           1,
+                                           0,
+                                           'i'),
+                            sys.htf.escape_sc(wwv_flow.g_x01),
+                            1,
+                            1,
+                            'i');
   END IF;
-  L_SQL := L_SQL || 'FROM ( ' || L_LOV || ' ) '; 
+  l_sql := l_sql || 'FROM ( ' || l_lov || ' ) ';
   --
-  IF UPPER( NVL( P_ITEM.ATTRIBUTE_01, 'F' ) ) LIKE 'F%' THEN    
-    L_SQL := L_SQL || 'WHERE UPPER(' || L_REC_TAB( 1 ).COL_NAME || ' ) ';
-    L_SQL := L_SQL || ' LIKE  ''%' || UPPER(SYS.HTF.ESCAPE_SC ( WWV_FLOW.G_X01 )) || '%'' ';
+  IF wwv_flow.g_x02 IS NULL
+  THEN
+    IF upper(nvl(p_item.attribute_01,
+                 'F')) LIKE 'F%'
+    THEN
+      l_sql := l_sql || 'WHERE UPPER(' || l_rec_tab(1).col_name || ' ) ';
+      l_sql := l_sql || ' LIKE  ''%' ||
+               upper(sys.htf.escape_sc(wwv_flow.g_x01)) || '%'' ';
+    ELSE
+      l_sql := l_sql || 'WHERE ' || l_rec_tab(1).col_name;
+      l_sql := l_sql || ' LIKE ''%' || sys.htf.escape_sc(wwv_flow.g_x01) ||
+               '%'' ';
+    END IF;
+    --
+    IF p_item.attribute_07 IS NOT NULL
+    THEN
+      l_sql := l_sql || 'AND ROWNUM <= ' || p_item.attribute_07 || ' ';
+    END IF;
+    --
+    l_sql := l_sql || 'ORDER BY 2';
+    --
+    apex_util.json_from_sql(l_sql);
   ELSE
-    L_SQL := L_SQL || 'WHERE ' || L_REC_TAB( 1 ).COL_NAME;
-    L_SQL := L_SQL || ' LIKE ''%' || SYS.HTF.ESCAPE_SC ( WWV_FLOW.G_X01 ) || '%'' ';
+    l_sql := l_sql || 'WHERE ' || l_rec_tab(2).col_name;
+    l_sql := l_sql ||
+             ' in (select cast(column_value as varchar2(400)) col from xmltable(''' ||
+             sys.htf.escape_sc(wwv_flow.g_x02) || ''')) ';
+    --
+    l_sql := l_sql || 'ORDER BY 2';
+    --
+    apex_util.json_from_sql(l_sql);
   END IF;
   --
-  IF P_ITEM.ATTRIBUTE_07 IS NOT NULL THEN
-    L_SQL := L_SQL || 'AND ROWNUM <= ' || P_ITEM.ATTRIBUTE_07 || ' ' ;
-  END IF;
+  RETURN l_return;
   --
-  L_SQL := L_SQL || 'ORDER BY 2';
-  --
-  APEX_UTIL.JSON_FROM_SQL ( L_SQL ) ;
-  --
-  RETURN L_RETURN;
-  --
-END AJAX;
+END ajax;
